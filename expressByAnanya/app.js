4a5b7feec919eb5e3a31f01a0382f790a0cce76f
Ananya <ananyaagrawal073@gmail.com>
2024-01-23 15:18:24 +0530

    commit
    commit

---
+++ expressByAnanya/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/about', (req, res) => {    
+    res.send('Hello World from about page');
+}
+);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});     // '/' is the root route                // req is the request object and res is the response object
+// Thd callback function has 2 parameters, requst(req) and response(res).
+// The request object(req) represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, etc.
+// Similarly, the response object represents the HTTP response that the Express app sends when it receives an HTTP request.
+app.listen(8000, () => console.log('Listening on port 8000...'));
